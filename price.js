d8350b15a8cd6019c317b50b6b7c7f28aaf2144b
unknown <longquydoan2020@gmail.com>
2022-08-09 16:44:44 +0700

    hoa qua

---
+++ price.js
@@ -0,0 +1,23 @@
+// if (price == "Ổi") {
+//                 document.write("Ổi")
+//             }
+//             else if (price == "Dưa Hấu") {
+//                 document.write("Dưa Hấu")
+//             }
+//             else if (price == "Táo") {
+//                 document.write("Táo")
+//             }
+//             else if (price == "Xoài") {
+//                 document.write("Xoài")
+//             }
+//             else if (price == "Cam") {
+//                 document.write("Cam")
+//             }
+//             else if (price == "Chôm Chôm") {
+//                 document.write("Chôm Chôm")
+//             }
+//             else if (price == "Măng Cụt") {
+//                 document.write("Măng Cụt")
+//             }
+//             else { document.write("Khong co") }
+            
